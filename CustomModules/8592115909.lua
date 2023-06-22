fa1d17ab107bab3fa691482122c2777ea7f39e99
ImNicknamez <136889440+ImNicknamez@users.noreply.github.com>
2023-06-22 13:22:08 +0200

    Add files via upload

---
+++ CustomModules/8592115909.lua
@@ -0,0 +1,19 @@
+local function vapeGithubRequest(scripturl)
+	if not isfile("FapeClient/"..scripturl) then
+		local suc, res = pcall(function() return game:HttpGet("https://raw.githubusercontent.com/ImNicknamez/FapeClient/"..readfile("FapeClient/commithash.txt").."/"..scripturl, true) end)
+		if not suc or res == "404: Not Found" then return nil end
+		if scripturl:find(".lua") then res = "--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.\n"..res end
+		writefile("FapeClient/"..scripturl, res)
+	end
+	return readfile("FapeClient/"..scripturl)
+end
+
+shared.CustomSaveVape = 8542275097
+if pcall(function() readfile("FapeClient/CustomModules/8542275097.lua") end) then
+	loadstring(readfile("FapeClient/CustomModules/8542275097.lua"))()
+else
+	local publicrepo = vapeGithubRequest("CustomModules/8542275097.lua")
+	if publicrepo then
+		loadstring(publicrepo)()
+	end
+end
